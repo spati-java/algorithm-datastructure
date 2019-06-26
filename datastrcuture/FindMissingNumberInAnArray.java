17b02bfcce337a2ed04403aae17a7f87494b2245
Sourav Pati <sourav.pati09@gmail.com>
2019-06-26 15:18:56 -0500

    added some space

--- datastrcuture/FindMissingNumberInAnArray.java
+++ datastrcuture/FindMissingNumberInAnArray.java
@@ -1,30 +1,28 @@
-public class FindMissingNumberInAnArray{
+public class FindMissingNumberInAnArray {
 
-    public static void main(String [] args){
-        int [] missingNumberArray = {1,2,3,4,5,6,7,8,9,10,11,12,14,13,15,16,19,17,20};
+    public static void main(String[] args) {
+        int[] missingNumberArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 13, 15, 16, 19, 17, 20 };
 
         findMissingNumber(missingNumberArray);
     }
 
-    public static  void findMissingNumber(int [] array){
+    public static void findMissingNumber(int[] array) {
 
-        int sum = 0; 
-        int expectedSum = 0; 
+        int sum = 0;
+        int expectedSum = 0;
         int missingNumber = 0;
 
-        for(int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
 
-            sum+=array[i];
+            sum += array[i];
 
         }
 
-        for(int j = 1; j <=20; j++){
-
-            expectedSum +=j;
+        for (int j = 1; j <= 20; j++) {
+            expectedSum += j;
         }
 
         missingNumber = expectedSum - sum;
-
 
         System.out.println(missingNumber);
 
