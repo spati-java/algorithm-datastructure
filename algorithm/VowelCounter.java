a0837a231b0f448d8a39a8820f336caefbb05897
Sourav Pati <sourav.pati09@gmail.com>
2019-06-26 16:25:09 -0500

    added a check

--- algorithm/VowelCounter.java
+++ algorithm/VowelCounter.java
@@ -3,28 +3,28 @@ public class VowelCounter {
     public static void main(String[] args) {
 
         String text = "Hello world I am a disco dancer";
-          countVowel(text);
+        countVowel(text);
 
-          System.out.print(countVowel(text));
+        System.out.print(countVowel(text));
     }
 
-    public static int countVowel(String text){
-              int count = 0;
-              char [] charArray = text.toCharArray();
-                for(char c: charArray){
+    public static int countVowel(String text) {
+        int count = 0;
+        char[] charArray = text.toLowerCase().toCharArray();
+        for (char c : charArray) {
 
-                    switch(c){
-                        case 'a':
-                        case 'e':
-                        case 'i':
-                        case 'o':
-                        case 'u':
-                         count ++;
-                    break;
-                    default: 
-                    }
-                }
-                return count;
+            switch (c) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                count++;
+            
+            default:
+            }
         }
+        return count;
+    }
 
 }
