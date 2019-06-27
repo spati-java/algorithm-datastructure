af2dbb5be65cde3b273bb2719000c3127ccf9cfa
Sourav Pati <sourav.pati09@gmail.com>
2019-06-27 17:16:49 -0500

    added kths smallest element

---
+++ datastrcuture/ReturnKthSmallestelement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class  ReturnKthSmallestelement{
+
+
+    public static void main(String [] args) {
+        int [] arra = {7, 10, 4, 3, 20, 15};
+        System.out.println(findKathsSmallestElm(arra,3));
+    }
+
+    public static int findKathsSmallestElm(int [] array, int k){
+
+        Arrays.sort(array);
+        return array[k-1];
+    }
+}
